d1cc4d46f6e58931516a18cf5c6e4bcab008a59a
Dragoon1010 <jonathan_renard@hotmail.fr>
2020-05-10 22:02:21 +0200

    fix: fix undefined value for voiceStateUpdate

--- src/events/voice-state-change.ts
+++ src/events/voice-state-change.ts
@@ -7,13 +7,12 @@ module.exports = {
 
     execute: (bot: Bot, oldState: VoiceState, newState: VoiceState) => {
         if (oldState.member.user.bot) return;
-
         bot.users.fetch(oldState.member.user.id).then((user: User) => {
-            if (oldState.channelID === null && newState.channelID !== null) {
+            if (!oldState.channelID && newState.channelID) {
                 bot.channels.fetch(newState.channelID).then((channel: TextChannel) => {
                     bot.writeLog(EmbedType.START_VOICE_CONNECTION, user, `${user.username} vient de se connecter au channel **${channel.name}**.`);
                 });
-            } else if (oldState.channelID !== null && newState.channelID === null) {
+            } else if (oldState.channelID && !newState.channelID) {
                 bot.channels.fetch(oldState.channelID).then((channel: TextChannel) => {
                     bot.writeLog(EmbedType.END_VOICE_CONNECTION, user, `${user.username} vient de se déconnecter du channel **${channel.name}**.`);
                 });
